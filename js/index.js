e89050b1821e588b3af335da29fe8b37c64f28a4
Francisco Nieves <fnievesc@gmail.com>
2013-11-12 23:07:05 -0600

    version 1.0.1

--- js/index.js
+++ js/index.js
@@ -17,33 +17,22 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
+
+
+$(document).bind('pagechange',function(toPage, options){
+    var url = $.mobile.path.parseUrl(toPage.currentTarget.URL);
+
+    switch(url.filename)
+    {
+        case 'jn.html':
+            window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
+            $( "#mySwipe" ).on( "swipeleft", function(){
+                mySwipe.next();
+            } );
+            $( "#mySwipe" ).on( "swiperight", function(){
+                mySwipe.next();
+            } );
+        break;
+    }
+});
